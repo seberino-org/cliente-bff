6e1ada29a7e9fc56dccd4b71641d539905d828d1
Richard Marques <richard@seberino.com.br>
2021-08-23 14:50:15 -0300

    Ajuste do Health Check

--- src/main/java/com/ibm/sample/cliente/bff/health/ClienteRestAPI.java
+++ src/main/java/com/ibm/sample/cliente/bff/health/ClienteRestAPI.java
@@ -31,29 +31,8 @@ public class ClienteRestAPI implements HealthIndicator {
 		int ponto=0;
 		try
 		{
-			logger.debug("Criando mensagem sintetica para teste da API Rest");
-			cliente.setCpf(123L);
-			cliente.setNome("CLIENTE SINTETICO - HEALTH CHECK");
-			cliente.setNumero(10);
-			cliente.setNasc(new java.util.Date());
-			cliente.setCidade("Cidade 1");
-			cliente.setComplemento(" ");
-			cliente.setLogradouro("Rua teste");
-			cliente.setMae("Mae teste");
-			cliente.setUf("SP");
-			cliente.setCep("123442");
-			logger.debug("Fazendo chamada Post na API Rest para gravar cliente sintetico");
-			clienteRestHealth.postForObject(urlClienteRest,cliente, RetornoCliente.class);
-			logger.debug("POST realizado com sucesso, aguardando 200 ms para fazer a consulta");
-			ponto=1;
-			//aguarda o processamento asincrino
-			Thread.sleep(400);
+
 			clienteRestHealth.getForObject(urlClienteRest + "/" + cliente.getCpf(), RetornoCliente.class);
-			logger.debug("Consulta do cliente recem cadastrado efetuada com sucesso! Testando a deleção");
-			ponto=2;
-			clienteRestHealth.delete(urlClienteRest + "/" + cliente.getCpf());
-			logger.debug("Deleção do cliente sintetico efetuada com sucesso");
-			ponto=3;
 			logger.debug("ClienteRestAPI esta saudável");
 			return Health.up().build();
 			
@@ -61,17 +40,7 @@ public class ClienteRestAPI implements HealthIndicator {
 		catch (Exception e)
 		{
 			logger.error("ClienteRestAPI não esta saudável. Falha ao validar a saúde da RestAPI de Cliente: " + e.getMessage());
-			String mensagem = "Falha na inclusão de novos clientes: " + e.getMessage();
-			if (ponto >=1)
-			{
-				mensagem = "Falha na consulta de um cliente: " + e.getMessage();
-			}
-			else if (ponto >2)
-			{
-				mensagem = "Falha na exclusão de um cliente: " + e.getMessage();
-			}
-			logger.error("ClienteRestAPI não saudável: " + mensagem);
-			return Health.down().withDetail("Cliente-BFF Não saudável", mensagem).build();
+			return Health.down().withDetail("Cliente-BFF Não saudável","Falha ao consultar dados de cliente via REST").build();
 		}
 	}
 	
